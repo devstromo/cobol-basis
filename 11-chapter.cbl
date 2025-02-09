020123758deda5636c1225c8ab700db0b1d76e13
alemoles <alemolerio@gmail.com>
2025-02-08 21:13:16 -0300

    Update 11-chapter.cbl

--- 11-chapter.cbl
+++ 11-chapter.cbl
@@ -12,13 +12,8 @@
            01 Numb1 PIC 999 VALUE 15.
            01 Numb2 PIC 999 VALUE 50.
            01 SumResult  PIC 999 VALUE 000.
-           01 GreetingText PIC XXXX VALUE "Hi".
 
        PROCEDURE DIVISION.
-       ShowData.
-          DISPLAY Numb1.
-          DISPLAY Numb2.
-          DISPLAY GreetingText.
       *> This PARAGRAPH calculate the Numb1 and Numb2 sum
        CalculateSum.
           ADD Numb1 Numb2 GIVING SumResult.
