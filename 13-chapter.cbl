f865798de46171b98c82795ef573291bc5ff181c
alemoles <alemolerio@gmail.com>
2025-02-15 20:46:45 -0300

    Add decimal number

--- 13-chapter.cbl
+++ 13-chapter.cbl
@@ -4,8 +4,10 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
            01 NumberValue PIC 99 VALUE 1.
+           01 DecimalNumberValue PIC 99V99 VALUE 10.40.
        PROCEDURE DIVISION.
        SHOW-VALUE.
             DISPLAY NumberValue
+            DISPLAY DecimalNumberValue
             STOP RUN.
        END PROGRAM CHAPTER-13.
