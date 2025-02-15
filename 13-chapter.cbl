5ab6712e40967cf10df1830a782fef1478b45cc2
alemoles <alemolerio@gmail.com>
2025-02-15 20:50:29 -0300

    Add negative decimal number

--- 13-chapter.cbl
+++ 13-chapter.cbl
@@ -7,11 +7,13 @@
            01 DecimalNumberValue PIC 99V99 VALUE 10.40.
            01 DecimalNumberValue2 PIC 999V999 VALUE 100.405.
            01 NegativeNumberValue PIC S99 VALUE -81.
+           01 NegativeDecimalNumberValue PIC S99V99 VALUE -81.45.
        PROCEDURE DIVISION.
        SHOW-VALUE.
             DISPLAY NumberValue
             DISPLAY DecimalNumberValue
             DISPLAY DecimalNumberValue2
             DISPLAY NegativeNumberValue
+            DISPLAY NegativeDecimalNumberValue
             STOP RUN.
        END PROGRAM CHAPTER-13.
