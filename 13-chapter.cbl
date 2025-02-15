41b527253b9ebcfa6ca6f2a98e93ea337d0e58a7
alemoles <alemolerio@gmail.com>
2025-02-15 20:53:09 -0300

    Add long decimal number

--- 13-chapter.cbl
+++ 13-chapter.cbl
@@ -9,6 +9,7 @@
            01 NegativeNumberValue PIC S99 VALUE -81.
            01 NegativeDecimalNumberValue PIC S99V99 VALUE -81.45.
            01 LongNumberValue PIC 9(15) VALUE 12232333.
+           01 LongDecimalNumberValue PIC 9(8)V99 VALUE 12232333.23.
        PROCEDURE DIVISION.
        SHOW-VALUE.
             DISPLAY NumberValue
@@ -17,5 +18,6 @@
             DISPLAY NegativeNumberValue
             DISPLAY NegativeDecimalNumberValue
             DISPLAY LongNumberValue
+            DISPLAY LongDecimalNumberValue
             STOP RUN.
        END PROGRAM CHAPTER-13.
