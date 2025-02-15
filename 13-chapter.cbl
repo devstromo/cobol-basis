9609d747bdc036f5e08a2b80bae2fc07855bfbf6
alemoles <alemolerio@gmail.com>
2025-02-15 20:47:20 -0300

    Update chapter 13

--- 13-chapter.cbl
+++ 13-chapter.cbl
@@ -5,9 +5,11 @@
        WORKING-STORAGE SECTION.
            01 NumberValue PIC 99 VALUE 1.
            01 DecimalNumberValue PIC 99V99 VALUE 10.40.
+           01 DecimalNumberValue2 PIC 999V999 VALUE 100.405.
        PROCEDURE DIVISION.
        SHOW-VALUE.
             DISPLAY NumberValue
             DISPLAY DecimalNumberValue
+            DISPLAY DecimalNumberValue2
             STOP RUN.
        END PROGRAM CHAPTER-13.
