366b55aabf6162e7d859742e925731e5ca7fa766
alemoles <alemolerio@gmail.com>
2025-02-17 22:20:12 -0300

    Update chapter 14

--- 14-chapter.cbl
+++ 14-chapter.cbl
@@ -3,8 +3,14 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+          01 Numb1 PIC 99 VALUE 25.
+          01 Numb2 PIC 99 VALUE 15.
+
+          01 Result PIC 99 VALUE ZERO.
        PROCEDURE DIVISION.
+          CalculateAndShow.
+             COMPUTE Result = Numb1.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
+            DISPLAY Result
             STOP RUN.
        END PROGRAM CHAPTER-14.
