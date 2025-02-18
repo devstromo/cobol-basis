53cd7438698809535824a2ba47b38af70316a6c7
alemoles <alemolerio@gmail.com>
2025-02-17 22:28:05 -0300

    Add division operation

--- 14-chapter.cbl
+++ 14-chapter.cbl
@@ -7,9 +7,12 @@
           01 Numb2 PIC 99 VALUE 15.
 
           01 Result PIC 99 VALUE ZERO.
+          01 DivResult PIC 99 VALUE ZERO.
        PROCEDURE DIVISION.
           CalculateAndShow.
              COMPUTE Result = Numb1 + Numb2.
              DISPLAY "The Result is " Result ".".
+             COMPUTE DivResult = Numb1 / Numb2.
+             DISPLAY "The Div Result is " DivResult ".".
             STOP RUN.
        END PROGRAM CHAPTER-14.
