30f29e87719aa739696c74575828618d6fa7f106
alemoles <alemolerio@gmail.com>
2025-02-22 22:50:22 -0300

    Division decimals trunk

--- 15-chapter.cbl
+++ 15-chapter.cbl
@@ -10,11 +10,16 @@
       *>  Nums vars
            01 Numb1 PIC 99V99 VALUE 10,55.
            01 Numb2 PIC 99V99 VALUE 65,20.
+           01 Numb3 PIC 99 VALUE 10.
+           01 Numb4 PIC 99 VALUE 3.
       *>  Result var
            01 ResultValue PIC S99V99 VALUE ZERO.
+           01 ResultDivValue PIC 9V99 VALUE ZERO.
        PROCEDURE DIVISION.
        SHOW-RESULT.
             COMPUTE ResultValue = Numb1- Numb2.
-            DISPLAY ResultValue
+            DISPLAY ResultValue.
+            COMPUTE ResultDivValue = Numb3/Numb4.
+            DISPLAY ResultDivValue.
             STOP RUN.
        END PROGRAM CHAPTER-15.
