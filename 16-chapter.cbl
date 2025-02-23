98a204ff22ed779cd5eee7f98bc8f4f8bf07b717
alemoles <alemolerio@gmail.com>
2025-02-22 23:22:35 -0300

    Multi sum operation

--- 16-chapter.cbl
+++ 16-chapter.cbl
@@ -9,10 +9,22 @@
        WORKING-STORAGE SECTION.
            01 Numb1 PIC 99 VALUE 10.
            01 Numb2 PIC 99 VALUE 3.
+           01 Numb3 PIC 99V99 VALUE 10,55.
+           01 Numb4 PIC 99V99 VALUE 0.
+           01 Numb5 PIC 99V99 VALUE 0.
+           01 Numb6 PIC 99V99 VALUE 0.
            01 ResultValue PIC 99 VALUE ZERO.
+           01 ResultOperValue PIC S9999V99 VALUE ZEROES.
        PROCEDURE DIVISION.
        SHOW-RESULT.
             ADD Numb1 TO Numb2 GIVING ResultValue.
+
             DISPLAY ResultValue.
+            ADD Numb3 TO Numb4, Numb5, Numb6.
+            DISPLAY ResultValue.
+            DISPLAY Numb3.
+            DISPLAY Numb4.
+            DISPLAY Numb5.
+            DISPLAY Numb6.
             STOP RUN.
        END PROGRAM CHAPTER-16.
