87773ccee24dc54589c2c0536ac858f0bacb7d9a
alemoles <alemolerio@gmail.com>
2025-02-22 22:44:12 -0300

    Update 15-chapter base

--- 15-chapter.cbl
+++ 15-chapter.cbl
@@ -3,8 +3,14 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+      *>  Nums vars
+           01 Numb1 PIC 99V99 VALUE 10.55.
+           01 Numb2 PIC 99V99 VALUE 65.20.
+      *>  Result var
+           01 ResultValue PIC S99V99 VALUE ZERO.
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            DISPLAY "Hello world"
+       SHOW-RESULT.
+            COMPUTE ResultValue = Numb1- Numb2.
+            DISPLAY ResultValue
             STOP RUN.
        END PROGRAM CHAPTER-15.
