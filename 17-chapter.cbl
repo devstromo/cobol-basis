89473db80943d35280a14404ddfcff8651ca86c5
alemoles <alemolerio@gmail.com>
2025-02-24 20:46:20 -0300

    Request input data

--- 17-chapter.cbl
+++ 17-chapter.cbl
@@ -4,11 +4,14 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
       *>  User data vars
-           01 Nombre PIC X(16) VALUES SPACES.
-           01 Apellidos PIC X(32) VALUES SPACES.
-           01 Edad PIC X(3) VALUES SPACES.
+           01 NameValue PIC X(16) VALUES SPACES.
+           01 LastnameValue PIC X(32) VALUES SPACES.
+           01 Age PIC X(3) VALUES SPACES.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
+      *>     Request user data
+           REQUEST-DATA.
+              DISPLAY "Input your name:"
+              ACCEPT NameValue.
             STOP RUN.
        END PROGRAM CHAPTER-17.
