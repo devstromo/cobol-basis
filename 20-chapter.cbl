2adefa58fa53c3d3113d6ce295be0d50cf816c9b
alemoles <alemolerio@gmail.com>
2025-03-08 21:10:46 -0300

    Using IF conditional

--- 20-chapter.cbl
+++ 20-chapter.cbl
@@ -4,7 +4,12 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
+       Conditionals.
+           IF 10 > 50 THEN
+               DISPLAY "First if is executed"
+           END-IF.
+          IF 10 < 50 THEN
+               DISPLAY "Second if is executed"
+           END-IF.
+           STOP RUN.
        END PROGRAM CHAPTER-20.
