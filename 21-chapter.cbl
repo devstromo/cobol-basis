8792d4c4fcfdddf08f533bea021c1228630d50f7
alemoles <alemolerio@gmail.com>
2025-03-10 21:49:21 -0300

    Conditional Values for strings

--- 21-chapter.cbl
+++ 21-chapter.cbl
@@ -3,11 +3,15 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-           01 ValueVar PIC X VALUE "7".
+           01 ValueVar PIC X VALUE "A".
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             IF ValueVar IS NUMERIC THEN
                DISPLAY "Numeric If executed"
             END-IF.
+            IF ValueVar IS ALPHABETIC THEN
+               DISPLAY "ALPHABETIC If executed"
+            END-IF.
+
             STOP RUN.
        END PROGRAM CHAPTER-21.
