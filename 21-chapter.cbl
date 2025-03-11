02b00723f21ab664a5b8d467b91bad61cfe75361
alemoles <alemolerio@gmail.com>
2025-03-10 21:51:56 -0300

    Using NOT ALPHABETIC-UPPER in string eval

--- 21-chapter.cbl
+++ 21-chapter.cbl
@@ -3,7 +3,7 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-           01 ValueVar PIC X VALUE "A".
+           01 ValueVar PIC X VALUE "a".
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             IF ValueVar IS NUMERIC THEN
@@ -17,6 +17,9 @@
             END-IF.
             IF ValueVar IS ALPHABETIC-UPPER THEN
                DISPLAY "ALPHABETIC-UPPER If executed"
+            END-IF.
+            IF ValueVar IS NOT ALPHABETIC-UPPER THEN
+               DISPLAY "NOT ALPHABETIC-UPPER If executed"
             END-IF.
             STOP RUN.
        END PROGRAM CHAPTER-21.
