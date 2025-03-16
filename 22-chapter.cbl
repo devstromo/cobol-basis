5d39ccff49dd1f1005976169d7dc1d79741d0f76
alemoles <alemolerio@gmail.com>
2025-03-15 23:10:41 -0300

    Using group vars

--- 22-chapter.cbl
+++ 22-chapter.cbl
@@ -6,6 +6,8 @@
        01 NumberOne PIC S99 VALUE -25.
        01 GroupVar.
            02 NameValue PIC X(15) VALUES SPACES.
+           02 LastnameValue PIC X(30) VALUES SPACES.
+           02 PhoneValue PIC X(9) VALUES SPACES.
        PROCEDURE DIVISION.
             STOP RUN.
        END PROGRAM CHAPTER-22.
