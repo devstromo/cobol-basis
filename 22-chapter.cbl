9805b9d4cfb515b87a1006bee6171ff8b792ca20
alemoles <alemolerio@gmail.com>
2025-03-15 23:13:31 -0300

    Using nested group vars

--- 22-chapter.cbl
+++ 22-chapter.cbl
@@ -5,9 +5,11 @@
        WORKING-STORAGE SECTION.
        01 NumberOne PIC S99 VALUE -25.
        01 GroupVar.
-           02 NameValue PIC X(15) VALUES SPACES.
-           02 LastnameValue PIC X(30) VALUES SPACES.
-           02 PhoneValue PIC X(9) VALUES SPACES.
+           05 NameValue PIC X(15) VALUES SPACES.
+           05 LastnameValue PIC X(30) VALUES SPACES.
+           05 PhonesValue.
+              10 Mobile PIC X(9) VALUES SPACES.
+              10 WorkPhone PIC X(9) VALUES SPACES.
        PROCEDURE DIVISION.
             STOP RUN.
        END PROGRAM CHAPTER-22.
