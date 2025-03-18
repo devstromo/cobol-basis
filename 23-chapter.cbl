05968cd8cb272faf6c919f6430ba231742d0ce58
alemoles <alemolerio@gmail.com>
2025-03-17 21:32:43 -0300

    Adding 23-chapter.cbl

---
+++ 23-chapter.cbl
@@ -0,0 +1,24 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CHAPTER-23.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01 Age PIC 99 VALUE ZEROS.
+           88 Young VALUE 1 THRU 39.
+           88 Adult VALUE 40 THRU 65.
+           88 Elder VALUE 66 THRU 100.
+       PROCEDURE DIVISION.
+       CheckYourAge.
+           DISPLAY "Input your age:"
+           ACCEPT Age.
+           IF Young THEN
+               DISPLAY "You are young. " WITH NO ADVANCING
+           END-IF
+           IF Adult THEN
+               DISPLAY "You are an adult. " WITH NO ADVANCING
+           END-IF
+           IF Elder THEN
+               DISPLAY "You are an elder. " WITH NO ADVANCING
+           END-IF
+           STOP RUN.
+       END PROGRAM CHAPTER-23.
