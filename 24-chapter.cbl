87dcfcec68c1882a48708ea0b5aa24f82e9c5d08
alemoles <alemolerio@gmail.com>
2025-03-22 21:06:15 -0300

    Add 24-chapter.cbl

---
+++ 24-chapter.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CHAPTER-24.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 NumberOne PIC 99 VALUE 10.
+       77 NumberSecond PIC 99 VALUE 10.
+       PROCEDURE DIVISION.
+       LogicalOperators.
+            IF NumberOne > 5 AND NumberSecond < 11 THEN
+             DISPLAY "Se cumple la confición"
+            END-IF
+            STOP RUN.
+       END PROGRAM CHAPTER-24.
