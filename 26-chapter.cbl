f9c9bc8ff5824e053fef09ec37ddaec3ca25f70e
alemoles <alemolerio@gmail.com>
2025-03-29 21:40:56 -0300

    Using EVALUATE instead of multple ifs

--- 26-chapter.cbl
+++ 26-chapter.cbl
@@ -3,10 +3,7 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01 Age PIC 999 VALUE ZEROS.
-           88 Young VALUE 1 THRU 39.
-           88 Adult VALUE 40 THRU 65.
-           88 Elder VALUE 66 THRU 100.
+       77 Age PIC 999 VALUE ZEROS.
        PROCEDURE DIVISION.
        AGE-CHECKER.
        DISPLAY "Input your age"
