bf7deb0dc4b7cdb66c356dc10dd3674ee9531c22
alemoles <alemolerio@gmail.com>
2025-04-01 21:38:25 -0300

    Using evaluate TRUE with multiple conditions

--- 27-chapter.cbl
+++ 27-chapter.cbl
@@ -7,6 +7,9 @@
            88 Young VALUE 1 THRU 39.
            88 Adult VALUE 40 THRU 65.
            88 Elder VALUE 66 THRU 100.
+       77 NumberOne PIC 9(5) VALUE 16.
+       77 NumberTwo PIC 9(5) VALUE 10.
+       77 NumberThree PIC 9(5) VALUE 20.
        PROCEDURE DIVISION.
         CheckYourAge.
            DISPLAY "Input your age:"
@@ -18,5 +21,19 @@
             DISPLAY "You're an adult. " WITH NO ADVANCING
             WHEN Elder
             DISPLAY "You're an elder. " WITH NO ADVANCING
-            STOP RUN.
+           END-EVALUATE.
+           DISPLAY "".
+       EvaluateValues.
+
+       EVALUATE TRUE
+           WHEN NumberOne = 16
+            DISPLAY "Block 1"
+           WHEN NumberTwo = 10
+            DISPLAY "Block 2 "
+           WHEN NumberThree >= 15
+            DISPLAY "Block 3 "
+           WHEN OTHER
+            DISPLAY "Value not found"
+            END-EVALUATE.
+           STOP RUN.
        END PROGRAM CHAPTER-27.
