ebabdfb21adb5577c3bd09538f3aa21f25b88012
alemoles <alemolerio@gmail.com>
2025-04-03 21:02:46 -0300

    Add 28-chapter.cbl

---
+++ 28-chapter.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CHAPTER-28.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 NumberOne PIC 999 VALUE 200.
+       77 NumberTwo PIC 999 VALUE 100.
+       77 ResultValue PIC 999 VALUE ZEROS.
+       PROCEDURE DIVISION.
+       CALCULATION-PROCEDURE.
+            COMPUTE ResultValue =  NumberOne + NumberTwo.
+            DISPLAY ResultValue.
+            STOP RUN.
+       END PROGRAM CHAPTER-28.
