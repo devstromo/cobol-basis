a2a17a2fc2ca05c4eb0132dc94d914ceeef535da
alemoles <alemolerio@gmail.com>
2025-04-05 17:52:11 -0300

    Adding 29-chapter.cbl

---
+++ 29-chapter.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CHAPTER-29.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 NumberOne PIC 99V999 VALUE 10.556.
+       77 NumberTwo PIC 99 VALUE 20.
+       77 ResultValue PIC 99V99 VALUE ZEROS.
+       PROCEDURE DIVISION.
+       CALCULATION-PROCEDURE.
+            COMPUTE ResultValue = NumberOne + NumberTwo.
+            DISPLAY ResultValue.
+            STOP RUN.
+       END PROGRAM CHAPTER-29.
