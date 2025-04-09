6923687913c21d1615c19fe2b798878e3de69bf9
alemoles <alemolerio@gmail.com>
2025-04-08 21:12:52 -0300

    Using ON SIZE ERROR in COMPUTE

--- 30-chapter.cbl
+++ 30-chapter.cbl
@@ -6,6 +6,7 @@
        77 ResultValue1 PIC 99V99 VALUES ZEROS.
        77 ResultValue2 PIC 9999 VALUES ZEROS.
        77 ResultValue3 PIC 99V99 VALUES ZEROS.
+       77 ResultValue4 PIC 9 VALUES ZEROS.
        PROCEDURE DIVISION.
        META-LANGUAGE.
             COMPUTE ResultValue1 ROUNDED, ResultValue2 ROUNDED,
@@ -13,5 +14,7 @@
             DISPLAY ResultValue1.
             DISPLAY ResultValue2.
             DISPLAY ResultValue3.
+            COMPUTE ResultValue4 ROUNDED = 10
+            ON SIZE ERROR DISPLAY "Something went wrong"
             STOP RUN.
        END PROGRAM CHAPTER-30.
