e81f91142b820097a33ff771f365b45a82130212
alemoles <alemolerio@gmail.com>
2025-04-10 21:12:50 -0300

    Using IF with Identifiers

--- 31-chapter.cbl
+++ 31-chapter.cbl
@@ -13,5 +13,11 @@
            IF Number2 IS GREATER THAN 25 THEN
             DISPLAY "Hello world"
            END-IF
+           IF Number1 NOT IS GREATER THAN 25 THEN
+            DISPLAY "Number1 is not greater than 25"
+           END-IF
+           IF 10 IS GREATER THAN 10 AND 10 IS GREATER THAN 5 THEN
+            DISPLAY "Hello world"
+           END-IF
             STOP RUN.
        END PROGRAM CHAPTER-31.
