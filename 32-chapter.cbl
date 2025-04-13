140e8471d7ca6c226ee03f67d00f5a2286d5d890
alemoles <alemolerio@gmail.com>
2025-04-13 13:48:44 -0300

    Add 32-chapter.cbl

---
+++ 32-chapter.cbl
@@ -0,0 +1,15 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CHAPTER-32.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 Number1 PIC S99 VALUES ZEROS.
+       77 Number2 PIC S99 VALUES ZEROS.
+       77 Number3 PIC S99 VALUES ZEROS.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            IF Number1 IS POSITIVE THEN
+                DISPLAY "Number1 is POSITIVE"
+            END-IF.
+            STOP RUN.
+       END PROGRAM CHAPTER-32.
