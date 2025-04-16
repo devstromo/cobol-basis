745572dc5626c28275f3ec6a89e4f9edd617d57e
alemoles <alemolerio@gmail.com>
2025-04-15 21:16:50 -0300

    Usin IS ALPHABETIC-UPPER in IF

--- 33-chapter.cbl
+++ 33-chapter.cbl
@@ -5,6 +5,7 @@
        WORKING-STORAGE SECTION.
        77 NumberValue PIC 99 VALUES 75.
        77 WebSite PIC X(25) VALUE "easy programming".
+       77 WebSiteUpper PIC X(25) VALUE "EASY PROGRAMMING".
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             IF NumberValue IS NUMERIC THEN
@@ -15,7 +16,12 @@
             IF WebSite IS ALPHABETIC-LOWER THEN
                 DISPLAY "The string is in lowercase"
             ELSE
-                DISPLAY "The string isn't  in lowercase"
+                DISPLAY "The string isn't in lowercase"
+            END-IF
+            IF WebSiteUpper IS ALPHABETIC-UPPER THEN
+                DISPLAY "The string is uppercase"
+            ELSE
+                DISPLAY "The string isn't is uppercase"
             END-IF
             STOP RUN.
        END PROGRAM CHAPTER-33.
