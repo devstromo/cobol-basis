0fe160fda888ef6090f815437bd1a92ce9671634
alemoles <alemolerio@gmail.com>
2025-04-15 21:25:35 -0300

    Usin custom class for numeric types

--- 33-chapter.cbl
+++ 33-chapter.cbl
@@ -1,5 +1,10 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. CHAPTER-33.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           CLASS Hexa IS "A" THRU "F", "0" THRU "9".
+           CLASS Decimal IS "0" THRU "9".
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
@@ -7,6 +12,8 @@
        77 WebSite PIC X(25) VALUE "easy programming".
        77 WebSiteUpper PIC X(25) VALUE "EASY PROGRAMMING".
        77 SaluteValue PIC AAAAA VALUE "Hello".
+       77 NumberOne PIC X(4) VALUE "A07F".
+       77 NumberTwo PIC 9(5) VALUE 41087.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             IF NumberValue IS NUMERIC THEN
@@ -33,6 +40,16 @@
                 DISPLAY "The value is alphabetic"
             ELSE
                 DISPLAY "The value isn't alphabetic"
+            END-IF
+            IF NumberOne IS Hexa THEN
+                DISPLAY "The value is hexa"
+            ELSE
+                DISPLAY "The value isn't hexa"
+            END-IF
+            IF NumberTwo IS Decimal THEN
+                DISPLAY "The value is decimal"
+            ELSE
+                DISPLAY "The value isn't decimal"
             END-IF
             STOP RUN.
        END PROGRAM CHAPTER-33.
