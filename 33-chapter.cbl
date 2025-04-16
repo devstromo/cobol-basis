2f8e2beb8601c42e328d50c6acf54a108675151f
alemoles <alemolerio@gmail.com>
2025-04-15 21:13:07 -0300

    Usin IS NUMERIC in IF

--- 33-chapter.cbl
+++ 33-chapter.cbl
@@ -3,10 +3,13 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       77 NumberValue PIC 99 VALUES 75.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            IF 10 IS NUMERIC THEN
+            IF NumberValue IS NUMERIC THEN
                 DISPLAY "It's a number"
+            ELSE
+                DISPLAY "It's not a number"
             END-IF
             STOP RUN.
        END PROGRAM CHAPTER-33.
