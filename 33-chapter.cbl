e90f4e75ab44a64d4db407953feeffbb20d2b644
alemoles <alemolerio@gmail.com>
2025-04-15 21:15:16 -0300

    Usin IS ALPHABETIC-LOWER in IF

--- 33-chapter.cbl
+++ 33-chapter.cbl
@@ -4,12 +4,18 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        77 NumberValue PIC 99 VALUES 75.
+       77 WebSite PIC X(25) VALUE "easy programming".
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             IF NumberValue IS NUMERIC THEN
                 DISPLAY "It's a number"
             ELSE
                 DISPLAY "It's not a number"
+            END-IF
+            IF WebSite IS ALPHABETIC-LOWER THEN
+                DISPLAY "The string is in lowercase"
+            ELSE
+                DISPLAY "The string isn't  in lowercase"
             END-IF
             STOP RUN.
        END PROGRAM CHAPTER-33.
