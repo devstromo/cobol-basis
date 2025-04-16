dafc2ce8b27af5ff2a3d6ca6c18fbd5a7d568ee0
alemoles <alemolerio@gmail.com>
2025-04-15 21:19:09 -0300

    Usin IS NOT NUMERIC in IF

--- 33-chapter.cbl
+++ 33-chapter.cbl
@@ -6,6 +6,7 @@
        77 NumberValue PIC 99 VALUES 75.
        77 WebSite PIC X(25) VALUE "easy programming".
        77 WebSiteUpper PIC X(25) VALUE "EASY PROGRAMMING".
+       77 SaluteValue PIC AAAAA VALUE "Hello".
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             IF NumberValue IS NUMERIC THEN
@@ -22,6 +23,11 @@
                 DISPLAY "The string is uppercase"
             ELSE
                 DISPLAY "The string isn't is uppercase"
+            END-IF
+            IF SaluteValue IS NOT NUMERIC THEN
+                DISPLAY "The value isn't numeric"
+            ELSE
+                DISPLAY "The value is numeric"
             END-IF
             STOP RUN.
        END PROGRAM CHAPTER-33.
