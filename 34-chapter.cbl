24f568a4f53aff60306ac8b4b32eddd82b824932
alemoles <alemolerio@gmail.com>
2025-04-17 21:28:21 -0300

    Add 34-chapter.cbl

--- 34-chapter.cbl
+++ 34-chapter.cbl
@@ -3,6 +3,14 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01 WeekDays PIC 9 VALUE ZERO.
+           88 Monday VALUE 1.
+           88 Tuesday VALUE 2.
+           88 Wednesday VALUE 3.
+           88 Thursday VALUE 4.
+           88 Friday VALUE 5.
+           88 Saturday VALUE 6.
+           88 Sunday VALUE 7.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
