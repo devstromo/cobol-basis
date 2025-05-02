c253aaba2a9a06184f5e4eb86c5e9d66973e9269
alemoles <alemolerio@gmail.com>
2025-05-01 22:12:46 -0300

    Using PERFORM to decrement variable value

--- 40-chapter.cbl
+++ 40-chapter.cbl
@@ -3,13 +3,13 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       77 NumberValue PIC 9 VALUE ZERO.
+       77 NumberValue PIC 9 VALUE 8.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             PERFORM Operation 7 TIMES.
             STOP RUN.
             Operation.
-            ADD 1 TO NumberValue.
+            SUBTRACT 1 FROM NumberValue.
             DISPLAY NumberValue.
 
        END PROGRAM CHAPTER-40.
