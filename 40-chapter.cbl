4885345d8245b9428c5cd00f300a21d9fdf6049b
alemoles <alemolerio@gmail.com>
2025-05-01 22:15:51 -0300

    Using PERFORM to increment variable value by multiply

--- 40-chapter.cbl
+++ 40-chapter.cbl
@@ -3,13 +3,13 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       77 NumberValue PIC 9 VALUE 8.
+       77 NumberValue PIC 9999 VALUE 8.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             PERFORM Operation 7 TIMES.
             STOP RUN.
             Operation.
-            SUBTRACT 1 FROM NumberValue.
+            MULTIPLY 2 BY NumberValue.
             DISPLAY NumberValue.
 
        END PROGRAM CHAPTER-40.
