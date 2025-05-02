83f5c5de5702319af13765d73207a2cf49c1558a
alemoles <alemolerio@gmail.com>
2025-05-01 22:11:49 -0300

    Using PERFORM to increment variable value

--- 40-chapter.cbl
+++ 40-chapter.cbl
@@ -3,9 +3,13 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       77 NumberValue PIC 9.
+       77 NumberValue PIC 9 VALUE ZERO.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-
+            PERFORM Operation 7 TIMES.
             STOP RUN.
+            Operation.
+            ADD 1 TO NumberValue.
+            DISPLAY NumberValue.
+
        END PROGRAM CHAPTER-40.
