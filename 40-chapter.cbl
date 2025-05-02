4bcfef9bd0aba9bd0aea5261fc03ad3a4a5ab97b
alemoles <alemolerio@gmail.com>
2025-05-01 22:22:10 -0300

    Using PERFORM to decrement variable value by division

--- 40-chapter.cbl
+++ 40-chapter.cbl
@@ -3,13 +3,15 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       77 NumberValue PIC 9999 VALUE 8.
+       77 NumberValue PIC 9999 VALUE 7000.
+       77 ResultValue PIC 9999 VALUE ZERO.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             PERFORM Operation 7 TIMES.
             STOP RUN.
             Operation.
-            MULTIPLY 2 BY NumberValue.
+            DIVIDE NumberValue by 2 GIVING ResultValue.
+            SUBTRACT ResultValue FROM NumberValue;
             DISPLAY NumberValue.
 
        END PROGRAM CHAPTER-40.
