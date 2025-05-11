8b3b11d052671a1ce81908eabb0ea76d9a8a4968
alemoles <alemolerio@gmail.com>
2025-05-10 23:33:49 -0300

    Add 43-chapter.cbl

---
+++ 43-chapter.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CHAPTER-43.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 NumberValue PIC 99 VALUE ZEROS.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Input number:"
+            ACCEPT NumberValue.
+            PERFORM SHOW-NUMBER WITH TEST BEFORE
+            UNTIL NumberValue = 10.
+       STOP RUN.
+
+       SHOW-NUMBER.
+       DISPLAY NumberValue.
+       ADD 1 TO NumberValue.
+       END PROGRAM CHAPTER-43.
